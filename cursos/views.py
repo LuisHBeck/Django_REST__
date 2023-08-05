9787b136b0f6a922c5bda4163665c79de11e4d31
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-05 16:37:40 -0300

    feat: implement api view get method

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    """
+    API de Cursos
+    """
+
+    def get(self, request):
+        print(request.user)
+        print(request.user.id)
+
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+
+class AvaliacaoAPIView(APIView):
+    """
+    API de Avaliações 
+    """
+
+    def get(self, request):
+        print(request) 
+
+        avalicoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avalicoes, many=True)
+        return Response(serializer.data)
+        
