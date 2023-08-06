27791da7b19a638978c4baa37e46232b99e34f33
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-06 14:57:45 -0300

    feat: implement api view post method

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,4 +4,4 @@ from .views import CursoAPIView, AvaliacaoAPIView
 urlpatterns = [
     path('cursos/', CursoAPIView.as_view(), name='cursos'),
     path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
-]
+] 
--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -10,13 +11,18 @@ class CursoAPIView(APIView):
     """
 
     def get(self, request):
-        print(request.user)
         print(request.user.id)
-
+        # cursos = Curso.objects.get(pk=3)
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
     
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class AvaliacaoAPIView(APIView):
     """
@@ -24,9 +30,15 @@ class AvaliacaoAPIView(APIView):
     """
 
     def get(self, request):
-        print(request) 
-
+        # print(request) 
         avalicoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avalicoes, many=True)
         return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
         
