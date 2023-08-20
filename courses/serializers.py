3a08470621da8875778415296851a7e305681b7d
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-20 19:44:53 -0300

    fix: fiels + heritage

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,31 +2,31 @@ from rest_framework import serializers
 from .models import Course, Rating
 
 
-class RatingSerializers(serializers.Serializer):
+class RatingSerializers(serializers.ModelSerializer):
 
     class Meta:
         extra_kwargs = {
             'email': {'write_only': True}
         }
         model = Rating
-        fields = [
+        fields = (
             'id', 
             'name', 
             'email', 
             'course', 
             'comment', 
             'rating'
-        ]
+        )
         
 
-class CourseSerializer(serializers.Serializer):
+class CourseSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Course
-        fields = [
+        fields = (
             'id',
             'title',
             'url',
             'published',
             'active'
-        ]
+        )
