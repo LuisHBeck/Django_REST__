eccc3aafc63f5ee17ad7293204179dbcfee1ae26
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-21 09:10:45 -0300

    feat: generic views method override

--- courses/urls.py
+++ courses/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     # COURSES
     path('courses/', CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:course_pk>/ratings/', RatingsAPIView.as_view(), name='courses_ratings'),
+    path('courses/<int:course_pk>/ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='course_rating'),
     
     # RATINGS
     path('ratings/', RatingsAPIView.as_view(), name='ratings'),
-    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
+    path('ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='rating'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Course, Rating
 from .serializers import CourseSerializer, RatingSerializers
@@ -20,9 +21,28 @@ class RatingsAPIView(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
     
+    def get_queryset(self):
+        pk = self.kwargs.get('course_pk')
+        if pk:
+            return self.queryset.filter(course_id=pk)
+        return self.queryset.all()
+    
     
 # List, Update and Delete a specific rating
 class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
     
+    def get_object(self):
+        pk_course = self.kwargs.get('course_pk')
+        pk_rating = self.kwargs.get('rating_pk')
+        if pk_course:
+            return get_object_or_404(
+                self.get_queryset(), 
+                course_id=pk_course,
+                pk=pk_rating
+            )
+        return get_object_or_404(
+            self.get_queryset(), 
+            pk=pk_rating
+        )
