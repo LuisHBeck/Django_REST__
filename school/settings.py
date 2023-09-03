b96ab506b0f0d33476df1d736611ee6319edac74
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-02 22:10:45 -0300

    fix: settings

--- school/settings.py
+++ school/settings.py
@@ -34,9 +34,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # BEALTIFUL ADMIN
-    'jazzmin',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -168,51 +165,6 @@ REST_FRAMEWORK = {
     #     'user': '10/minute'
     # }
 }
-
-
-JAZZMIN_SETTINGS = {
-    "site_title": "Beck REST",
-    "welcome_sign": "Welcome back",
-    "site_header": "BECK",
-    "site_brand": " BECK ",
-    "copyright": "www.beck.com",
-    # "site_logo": "lebron.png",
-}
-
-JAZZMIN_UI_TWEAKS = { 
-    "navbar_small_text": False, 
-    "footer_small_text": False, 
-    "body_small_text": False, 
-    "brand_small_text": False, 
-    "brand_colour": "navbar-success", 
-    "accent": "accent-teal", 
-    "navbar": "navbar-dark", 
-    "no_navbar_border": False, 
-    "navbar_fixed": False, 
-    "layout_boxed": False, 
-    "footer_fixed": False, 
-    "sidebar_fixed": False, 
-    "sidebar": "sidebar-dark-info", 
-    "sidebar_nav_small_text": False, 
-    "sidebar_disable_expand": False, 
-    "sidebar_nav_child_indent": False, 
-    "sidebar_nav_compact_style": False, 
-    "sidebar_nav_legacy_style": False, 
-    "sidebar_nav_flat_style": False, 
-    # "theme": "cyborg", 
-    "theme": "solar", 
-    "dark_mode_theme": 'darkly', 
-    "button_classes": { 
-        "primary": "btn-primary", 
-        "secondary": "btn-secondary", 
-        "info": "btn-info", 
-        "warning": "btn-warning", 
-        "danger": "btn-danger", 
-        "success": "btn-success", 
-    }, 
-}
-
-
 
 # HEROKU CONFIGS
 django_heroku.settings(locals())
